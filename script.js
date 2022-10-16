1ca539c20b301146810a1c04e9db960eabe90b15
NolanRangel <rangelnolan9@gmail.com>
2022-10-16 14:58:12 -0600

    canvas updated

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-// const canvas = document.querySelector("#board canvas");
-// const ctx = canvas.getContext("2d");
-// console.log(ctx.canvas);
+const gameOn = true;
 
-function draw() {
-    const canvas = document.getElementById('canvas');
-    const { width: w, height: h } = canvas;
+const game = {};
+
+const player = {
+    left: 400,
+    top: 620,
+    velocity: {x: 0, y: 0},
+    score: 0,
+    life: 3,
+}
+
+game.canvas = document.getElementById('canvas');
+game.ctx = game.canvas.getContext('2d');
+
+game.backgroundColor = '#000000';
+game.canvas.width = document.documentElement.clientWidth - 20;
+
+game.update = function() {
+    game.ctx.fillStyle = game.backgroundColor;
+    game.ctx.fillRect(0, 0, game.canvas.width, game.canvas.height);
+}
+
+
+game.keydown = function(e) {
+      
+}      
     
-    if (canvas.getContext) {
-      const ctx = canvas.getContext('2d');
-  
-      ctx.fillStyle = "black";
-      ctx.fillRect(0, 0, w, h);
-      ctx.fillStyle = "#555";
-    }
-  }
+game.init = function() {
+      // Set the game loop
+    game.interval = setInterval(game.update, 1000 / 60);
+}
+
+
+game.stop = function() {
+    clearInterval(game.interval);
+}
+    
+
+game.restart = function() {
+    game.stop();
+    game.init();
+}
+    
+
+window.onload = game.init;
+
+
+window.onkeydown = game.keydown;
+
+
+
+ 
+
+ 
+
   
